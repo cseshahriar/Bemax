5b0c8caa490c17a2a05a692ca2c5c647be4dc392
shahriarcse <cse.shahriar.hosen@gmail.com>
2018-07-09 10:07:29 +0600

    Brand slide done

--- js/active.js
+++ js/active.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
                 items:1
             },
             600:{
-                items:1
+                items:1 
             },
             1000:{
                 items:1
             }
         }
-    })
+    });
     
     //project
     $('.project-active').owlCarousel({
@@ -35,7 +35,7 @@ $(document).ready(function() {
                 items:4
             }
         }
-    })
+    });
 
     //blog
     $('.blog-active').owlCarousel({
@@ -54,7 +54,47 @@ $(document).ready(function() {
                 items:1
             }
         }
-    })
+    });
+
+    //testimonial
+    $('.testimonial-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],  
+        dots: true,
+        responsive:{
+            0:{
+                items:1 
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    //brand
+    $('.brand-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],  
+        dots: true,
+        autoplay: true,
+        margin:50,
+        responsive:{
+            0:{
+                items:6
+            },
+            600:{
+                items:6
+            },
+            1000:{
+                items:6
+            }
+        }
+    }); 
 
     //video popup
     $('.video-popup').magnificPopup({ 
